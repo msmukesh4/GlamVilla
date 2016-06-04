bcabdea3938b23f6202855131d513df2aa48f0f8
msmukesh4 <msmukesh4@gmail.com>
2016-06-04 22:56:33 +0530

    first commit

---
+++ app/src/main/java/com/example/glamvilla/Common.java
@@ -0,0 +1,20 @@
+package com.example.glamvilla;
+
+/**
+ * Created by mukesh on 4/6/16.
+ */
+public class Common {
+
+    public static boolean IS_USER_LOGGED_IN = true;
+
+
+//    user info
+    public static String uuid = null;
+    public static String location = null;
+    public static String firstname = null;
+    public static String lastname = null;
+    public static String mobile_number = null;
+    public static String email = null;
+    public static boolean registered = false;
+
+}
